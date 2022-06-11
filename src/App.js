6bbbb002609e8c30d1ace83ca4fff0a4350c46c4
asif <masifabid@outlook.com>
2022-06-11 13:52:56 +0530

    code arranged

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom"
-import { Home } from "./components/HomePage";
-import { Login } from "./components/Login";
 import { useEffect } from "react";
-import { useAuthProvider } from "./authProvider";
-import { Signup } from "./components/SignUp";
-import { Post } from "./components/Post";
-import { PrivateRoute } from "./components/PrivateRoute";
-import { PostMaker } from "./components/PostMaker";
 import { Edit } from "./components/Edit";
-import { Reply } from "./components/Reply";
 import { User } from "./components/User";
+import { Post } from "./components/Post";
+import { Login } from "./components/Login";
+import { Reply } from "./components/Reply";
+import { Signup } from "./components/SignUp";
+import { Home } from "./components/HomePage";
+import { usePostProvider } from "./postProvider";
+import { useAuthProvider } from "./authProvider";
+import { Routes, Route } from "react-router-dom"
+import { PostMaker } from "./components/PostMaker";
 import { BookMarked } from "./components/BookMarked";
 import { LandingPage } from "./components/LandingPage";
-import { usePostProvider } from "./postProvider";
 import { getCall } from "./components/ReusableFunctions";
+import { PrivateRoute } from "./components/PrivateRoute";
+
 
 function App() {
   const { state: authState, dispatch: authDispatch } = useAuthProvider();
@@ -35,23 +36,31 @@ function App() {
   return (
     <div>
       <Routes>
-        {
-          authState.isLoggedIn ? <Route path="/" element={<Home />} /> : <Route path="/" element={<LandingPage />} />
-
-        }
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        {
-          authState.isLoggedIn ? <Route path="/login" element={<Home />} /> : <Route path="/login" element={<Login />} />
-        }
+        <Route path="/edit/:id" element={<Edit/>} />
+        <Route path="/reply/:id" element={<Reply/>} />
+        <Route path="/user/:username" element={<User/>} />
+        <Route path="/postmaker" element={<PostMaker/>} />
+        <Route path="/bookmarked" element={<BookMarked />} />
         <Route element={<PrivateRoute />}>
           <Route path="/post/:id" element={<Post />} />
         </Route>
-        <Route path="/user/:username" element={<User />} />
-        <Route path="/postmaker" element={<PostMaker />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/reply/:id" element={<Reply />} />
-        <Route path="/bookmarked" element={<BookMarked />} />
+        {
+          authState.isLoggedIn 
+          ? 
+          <Route path="/" element={<Home />} /> 
+          :
+          <Route path="/" element={<LandingPage />} />
+        }
+        {
+           authState.isLoggedIn 
+           ?
+           <Route path="/login" element={<Home />} /> 
+           :
+           <Route path="/login" element={<Login />} />
+        }
+        
       </Routes>
     </div>
 
