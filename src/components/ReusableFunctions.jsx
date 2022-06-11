79401252cb4e1c2c695c83224c0edfdeeab119b5
asif <masifabid@outlook.com>
2022-06-12 00:24:04 +0530

    delteCall() function added

--- src/components/ReusableFunctions.jsx
+++ src/components/ReusableFunctions.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
+const token = localStorage.getItem("encodedToken");
 
-export async function getCall(endPoint) {
+async function getCall(endPoint) {
   try {
-    const token = localStorage.getItem("encodedToken");
     const response = await axios.get(endPoint, {
       headers: {
         authorization: token,
       },
     });
-    if (response.status === 200) {
+    if (response.status === 200 || response.status === 201) {
       return response.data;
     }
   } catch (error) {
@@ -16,14 +16,26 @@ export async function getCall(endPoint) {
   }
 }
 
-export const postCall = async (endPoint, rqstBody) => {
-  const token = localStorage.getItem("encodedToken");
+const postCall = async (endPoint, rqstBody) => {
   const response = await axios.post(endPoint, rqstBody, {
     headers: {
       authorization: token,
     },
   });
-  if (response.status === 201) {
+  if (response.status === 200 || response.status === 201) {
     return response.data;
   }
 };
+
+const deleteCall = async (endPoint) => {
+  const response = await axios.delete(endPoint, {
+    headers: {
+      authorization: token,
+    },
+  });
+  if (response.status === 200 || response.status === 201) {
+    return response.data;
+  }
+};
+
+export { getCall, postCall, deleteCall };
