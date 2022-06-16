e37d027dec2a0e4104a0168709a3114c92420afe
asif <masifabid@outlook.com>
2022-06-16 11:00:22 +0530

    Navlist, clickables made with a tag & button tag

--- src/App.js
+++ src/App.js
@@ -6,65 +6,56 @@ import { Post } from "./components/Post";
 import { Login } from "./components/Login";
 import { Reply } from "./components/Reply";
 import { Signup } from "./components/SignUp";
-import { Home } from "./components/HomePage"; 
+import { Home } from "./components/HomePage";
 import { usePostProvider } from "./postProvider";
 import { useAuthProvider } from "./authProvider";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import { PostMaker } from "./components/PostMaker";
 import { BookMarked } from "./components/BookMarked";
 import { LandingPage } from "./components/LandingPage";
 import { getCall } from "./components/ReusableFunctions";
 import { PrivateRoute } from "./components/PrivateRoute";
 
-
 function App() {
   const { state: authState, dispatch: authDispatch } = useAuthProvider();
-  const { dispatch } = usePostProvider()
-  useEffect(async() => {
-    const token = localStorage.getItem("encodedToken")
+  const { dispatch } = usePostProvider();
+  useEffect(async () => {
+    const token = localStorage.getItem("encodedToken");
     if (token) {
-      authDispatch({ type: "LOGIN_STATUS", payload: true })
+      authDispatch({ type: "LOGIN_STATUS", payload: true });
     } else {
-      authDispatch({ type: "LOGIN_STATUS", payload: false })
+      authDispatch({ type: "LOGIN_STATUS", payload: false });
     }
-
     let data = await getCall("/api/users/");
-    dispatch({type: "GET_USERS", payload: data.users})
-  }, [])
-
+    dispatch({ type: "GET_USERS", payload: data.users });
+  }, []);
 
   return (
     <div>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/edit/:id" element={<Edit/>} />
-        <Route path="/reply/:id" element={<Reply/>} />
-        <Route path="/user/:username" element={<User/>} />
-        <Route path="/postmaker" element={<PostMaker/>} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/reply/:id" element={<Reply />} />
+        <Route path="/user/:username" element={<User />} />
+        <Route path="/postmaker" element={<PostMaker />} />
         <Route path="/bookmarked" element={<BookMarked />} />
         <Route element={<PrivateRoute />}>
           <Route path="/post/:id" element={<Post />} />
         </Route>
-        {
-          authState.isLoggedIn 
-          ? 
-          <Route path="/" element={<Home />} /> 
-          :
+        {authState.isLoggedIn ? (
+          <Route path="/" element={<Home />} />
+        ) : (
           <Route path="/" element={<LandingPage />} />
-        }
-        {
-           authState.isLoggedIn 
-           ?
-           <Route path="/login" element={<Home />} /> 
-           :
-           <Route path="/login" element={<Login />} />
-        }
-        
+        )}
+        {authState.isLoggedIn ? (
+          <Route path="/login" element={<Home />} />
+        ) : (
+          <Route path="/login" element={<Login />} />
+        )}
       </Routes>
     </div>
-
-  )
+  );
 }
 
 export default App;
--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,32 +1,41 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuthProvider } from "../authProvider";
 import "./NavList.css";
 export const NavList = () => {
+  const navigate = useNavigate();
+  const { state: authState } = useAuthProvider();
   return (
     <div className="navlist">
       <div className="navlist-content-container">
-        <div>
-          <Link to="/home" className="navlist-links">
-            <i className="fa-solid fa-house"></i>
-            Home
-          </Link>
-        </div>
-        <div>
-          <Link to="/bookmarked" className="navlist-links">
-            <i className="fa-solid fa-bookmark"></i>
-            Bookmarks
-          </Link>
-        </div>
-        <div>
-          <Link to="/login" className="navlist-links">
-            <i className="fa-solid fa-user"></i>
-            Profile
-          </Link>
-        </div>
-        <div>
-          <a className="navlist-btn" href="#createPostHere">
-            Create New Post
-          </a>
-        </div>
+        <a onClick={() => navigate("/home")}>
+          <i className="fa-solid fa-house"></i>
+          Home
+        </a>
+        <a
+          onClick={() =>
+            authState.isLoggedIn ? navigate("/bookmarked") : navigate("/login")
+          }
+        >
+          <i className="fa-solid fa-bookmark"></i>
+          Bookmarks
+        </a>
+        <a
+          onClick={() =>
+            authState.isLoggedIn ? navigate("/login") : navigate("/login")
+          }
+        >
+          <i className="fa-solid fa-user"></i>
+          Profile
+        </a>
+        <button
+          className="navlist-btn"
+          href="#createPostHere"
+          onClick={() =>
+            authState.isLoggedIn ? navigate("/login") : navigate("/login")
+          }
+        >
+          Create New Post
+        </button>
       </div>
     </div>
   );
