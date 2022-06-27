0f335d8ef42aba1ecd79473a5647d106804f83c7
asif <masifabid@outlook.com>
2022-06-27 21:52:09 +0530

    .

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,27 @@
 import "./User.css";
 import { Layout } from "./Layout";
 import { usePostProvider } from "../postProvider";
+import { useParams } from "react-router-dom";
+import { postCall } from "./ReusableFunctions";
+import { useState } from "react";
 
 export const User = () => {
-  const { state } = usePostProvider();
+  const { username } = useParams();
+  const { state, dispatch } = usePostProvider();
+  const foundUser = state.users.find((user) => user.username === username);
+  const [selectedUser, setSelectedUser] = useState(foundUser);
+  const [followed, setFollowed] = useState(false);
+
+  const followHandler = async (userId) => {
+    const data = await postCall(`/api/users/follow/${userId}`, {});
+    setSelectedUser(data.followUser);
+    setFollowed(true);
+  };
+  const unfollowHandler = async (userId) => {
+    const data = await postCall(`/api/users/unfollow/${userId}`, {});
+    setSelectedUser(data.followUser);
+    setFollowed(false);
+  };
   return (
     <Layout>
       <div className="img-container">
@@ -15,22 +33,35 @@ export const User = () => {
         />
       </div>
       <div className="btn-container">
-        <button
-          className="follow-btn duck-primary-btn-s duck-primary-btn"
-          // onClick={() => follow(user._id)}
-        >
-          Follow
-        </button>
+        {followed ? (
+          <button
+            className="follow-btn duck-primary-btn-s duck-primary-btn"
+            onClick={() => unfollowHandler(selectedUser._id)}
+          >
+            Unfollow
+          </button>
+        ) : (
+          <button
+            className="follow-btn duck-primary-btn-s duck-primary-btn"
+            onClick={() => followHandler(selectedUser._id)}
+          >
+            Follow
+          </button>
+        )}
       </div>
       <div className="userInfo-container">
         <p className="name-and-userId">
-          {/* <strong>{`${user.firstName} ${user.lastName}`}</strong> */}
-          {/* <small>{`@${user.username}`}</small> */}
+          <strong>{`${selectedUser.firstName} ${selectedUser.lastName}`}</strong>
+          <small>{`@${selectedUser.username}`}</small>
         </p>
         <p>Joined May 2022</p>
         <p>
-          <span>{/* <strong>{user.following}</strong>Following */}</span>
-          <span>{/* <strong>{user.followers}</strong>Followes */}</span>
+          <span>
+            <strong>{selectedUser.following.length}</strong>Following
+          </span>
+          <span>
+            <strong>{selectedUser.followers.length}</strong>Followers
+          </span>
         </p>
       </div>
     </Layout>
